0d5e60c32cfae44d0c1d1207fafca7ff0a8ce4d7
“BenCichos” <b.cichos@me.com>
2024-10-17 17:35:16 +0200

    Updating Approximate Relations

--- src/TraycerCore.jl
+++ src/TraycerCore.jl
@@ -1,22 +1,21 @@
 module TraycerCore
 
 using Refraction: Material, isnullmaterial, NULL_MATERIAL, transmittance
+export Material, transmittance
 using StaticArrays: SVector, SA, @SVector
+export SVector, SA, @SVector
 using LinearAlgebra: normalize, norm, dot, cross
 using Quaternions: Quaternion, imag_part, conj
 using Base: @kwdef
 
 using ApproximateRelations
 
-@init_approx! 1e-6
+@init_approx 1e-6
 
 import Quaternions: Quaternion
 import Refraction: transmittance
 import Base: show, convert, push!, append!, iterate, :(*), getproperty
 
-export SVector, SA, @SVector
-export Material, NULL_MATERIAL, isnullmaterial, transmittance
-export @approx, approx
 
 include("utils.jl")
 include("ray.jl")
@@ -30,6 +29,5 @@ include("minintersection.jl")
 include("trace.jl")
 include("recursivetrace.jl")
 include("box.jl")
-
 
 end
